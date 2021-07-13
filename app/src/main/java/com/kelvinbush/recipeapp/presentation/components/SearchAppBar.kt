f2e3904f66a539c627de3c41a7e7735ab31fb097
Kelvin Bush <kelybush@gmail.com>
2021-07-13 12:44:19 +0300

    state hoisting
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

---
+++ app/src/main/java/com/kelvinbush/recipeapp/presentation/components/SearchAppBar.kt
@@ -0,0 +1,99 @@
+package com.kelvinbush.recipeapp.presentation.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.kelvinbush.recipeapp.presentation.ui.recipe_list.FoodCategory
+import com.kelvinbush.recipeapp.presentation.ui.recipe_list.getAllFoodCategory
+
+@ExperimentalComposeUiApi
+@Composable
+fun SearchAppBar(
+    query: String, onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+    scrollPosition: Int,
+    selectedCategory: FoodCategory?,
+    onSelectedCategoryChanged: (String) -> Unit,
+    onChangeCategoryScrollPosition: (Int) -> Unit
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = Color.White,
+        elevation = 8.dp
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    value = query,
+                    onValueChange = { newValue ->
+                        onQueryChanged(newValue)
+                    },
+                    label = {
+                        Text(text = "Search")
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            Icons.Filled.Search,
+                            contentDescription = "Search Icon"
+                        )
+                    },
+                    keyboardActions = KeyboardActions(
+                        onSearch = {
+                            onExecuteSearch()
+                            keyboardController?.hide()
+                        }
+                    ),
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = MaterialTheme.colors.surface
+                    )
+                )
+            }
+            val state = rememberScrollState()
+            LaunchedEffect(Unit) { state.animateScrollTo(scrollPosition) }
+            Row(
+                modifier = Modifier
+                    .horizontalScroll(state = state)
+                    .padding(start = 8.dp, bottom = 8.dp, top = 8.dp),
+            ) {
+                for (category in getAllFoodCategory()) {
+                    FoodCategoryChip(
+                        category = category.value,
+                        isSelected = selectedCategory == category,
+                        onExecuteSearch = { onExecuteSearch() },
+                        onSelectedCategoryChanged = {
+                            onSelectedCategoryChanged(it)
+                            onChangeCategoryScrollPosition(state.value)
+                        }
+                    )
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/kelvinbush/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/kelvinbush/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -29,6 +29,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.kelvinbush.recipeapp.presentation.components.FoodCategoryChip
 import com.kelvinbush.recipeapp.presentation.components.RecipeCard
+import com.kelvinbush.recipeapp.presentation.components.SearchAppBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -50,69 +51,18 @@ class RecipeListFragment : Fragment() {
             setContent {
                 val recipes = viewModel.recipes.value
                 val query = viewModel.query.value
-                val keyboardController = LocalSoftwareKeyboardController.current
+
                 val selectedCategory = viewModel.selectedCategory.value
                 Column {
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        color = Color.White,
-                        elevation = 8.dp
-                    ) {
-                        Column(modifier = Modifier.fillMaxWidth()) {
-                            Row {
-                                TextField(
-                                    modifier = Modifier.fillMaxWidth(0.9f),
-                                    value = query,
-                                    onValueChange = { newValue ->
-                                        viewModel.onQueryChanged(newValue)
-                                    },
-                                    label = {
-                                        Text(text = "Search")
-                                    },
-                                    keyboardOptions = KeyboardOptions(
-                                        keyboardType = KeyboardType.Text,
-                                        imeAction = ImeAction.Search
-                                    ),
-                                    leadingIcon = {
-                                        Icon(
-                                            Icons.Filled.Search,
-                                            contentDescription = "Search Icon"
-                                        )
-                                    },
-                                    keyboardActions = KeyboardActions(
-                                        onSearch = {
-                                            viewModel.newSearch()
-                                            keyboardController?.hide()
-                                        }
-                                    ),
-                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                    colors = TextFieldDefaults.textFieldColors(
-                                        backgroundColor = MaterialTheme.colors.surface
-                                    )
-                                )
-                            }
-                            val state = rememberScrollState()
-                            LaunchedEffect(Unit) { state.animateScrollTo(viewModel.categoryScrollPosition) }
-                            Row(
-                                modifier = Modifier
-                                    .horizontalScroll(state = state)
-                                    .padding(start = 8.dp, bottom = 8.dp, top = 8.dp),
-                            ) {
-                                for (category in getAllFoodCategory()) {
-                                    FoodCategoryChip(
-                                        category = category.value,
-                                        isSelected = selectedCategory == category,
-                                        onExecuteSearch = viewModel::newSearch,
-                                        onSelectedCategoryChanged = {
-                                            viewModel.onSelectedCategoryChanged(it)
-                                            viewModel.onChangeCategoryScrollPosition(state.value)
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                    }
+                    SearchAppBar(
+                        query = query,
+                        onQueryChanged = viewModel::onQueryChanged,
+                        onExecuteSearch = viewModel::newSearch,
+                        scrollPosition = viewModel.categoryScrollPosition,
+                        selectedCategory = selectedCategory,
+                        onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
+                        onChangeCategoryScrollPosition = viewModel::onChangeCategoryScrollPosition
+                    )
                     LazyColumn {
                         itemsIndexed(items = recipes) { _, item ->
                             RecipeCard(recipe = item, onClick = {})
